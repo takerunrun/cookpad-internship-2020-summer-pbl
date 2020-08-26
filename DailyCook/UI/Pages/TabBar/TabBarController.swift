6b0a7b9c55cebfd824baa6f756257c31e91b3f3e
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-26 18:07:00 +0900

    update TabBarController

--- DailyCook/UI/Pages/TabBar/TabBarController.swift
+++ DailyCook/UI/Pages/TabBar/TabBarController.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TabBarController: UITabBarController {
+    private struct Const {
+        static let tabBarImages: [UIImage] = [#imageLiteral(resourceName: "home_empty"), #imageLiteral(resourceName: "user_empty")]
+        static let tabBarSelectedImages: [UIImage] = [#imageLiteral(resourceName: "home_filled"), #imageLiteral(resourceName: "user_filled")]
+    }
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewControllers()
+    }
+
+    // MARK: - Setup Methods
+    private func setupViewControllers() {
+        viewControllers = [
+            DevelopingViewController(type: "RecipeList"),
+            DevelopingViewController(type: "CookedRecipe"),
+        ]
+
+        tabBar.do {
+            $0.isTranslucent = false
+            $0.backgroundColor = Color.lightGray
+            $0.tintColor = Color.textBlack
+            $0.unselectedItemTintColor = Color.textGray
+            $0.items?.enumerated().forEach { index, tabBarItem in
+                tabBarItem.image = Const.tabBarImages[index]
+                tabBarItem.selectedImage = Const.tabBarSelectedImages[index]
+                tabBarItem.imageInsets = UIEdgeInsets(top: 4, left: 0, bottom: -4, right: 0)
+            }
+        }
+    }
+}
