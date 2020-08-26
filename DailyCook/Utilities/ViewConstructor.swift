8f00744da393298a32f64c62bf3aa4f540bac88e
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-26 17:34:32 +0900

    add ViewConstructor

---
+++ DailyCook/Utilities/ViewConstructor.swift
@@ -0,0 +1,12 @@
+//
+//  ViewConstructor.swift
+//  DailyCook
+//
+//  Created by admin on 2020/08/26.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+protocol ViewConstructor: AnyObject {
+    func setupViews()
+    func setupViewConstraints()
+}
