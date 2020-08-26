6288990f91beba76a9cb27fd2b4e7de705fe1aeb
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-26 17:25:07 +0900

    update UILabel+Extension

--- DailyCook/Extensions/UILabel+Extension.swift
+++ DailyCook/Extensions/UILabel+Extension.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum FontStyle: String {
+    case black = "NotoSansCJKjp-Black"
+    case bold = "NotoSansCJKjp-Bold"
+    case regular = "NotoSansCJKjp-Regular"
+    case medium = "NotoSansCJKjp-Medium"
+}
+
+extension UILabel {
+
+    func apply(fontStyle: FontStyle, size: CGFloat, color: UIColor = Color.textBlack) {
+        font = UIFont(name: fontStyle.rawValue, size: size)
+        textColor = color
+    }
+}
