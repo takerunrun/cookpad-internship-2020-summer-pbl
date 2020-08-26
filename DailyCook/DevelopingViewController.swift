5ac673019bbec6b78d70b1b9c21779ebc6df4f36
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-26 17:40:28 +0900

    update DevelopingViewController

--- DailyCook/DevelopingViewController.swift
+++ DailyCook/DevelopingViewController.swift
@@ -6,4 +6,47 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+final class DevelopingViewController: UIViewController, ViewConstructor {
+    
+    private let titleLabel = UILabel().then {
+        $0.text = "Developing ViewController"
+    }
+    
+    private let typeLabel = UILabel()
+    
+    init(type: String) {
+        super.init(nibName: nil, bundle: nil)
+        
+        typeLabel.text = type
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    func setupViews() {
+        view.addSubview(titleLabel)
+        view.addSubview(typeLabel)
+    }
+    
+    func setupViewConstraints() {
+        titleLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        typeLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
+        }
+    }
+}
