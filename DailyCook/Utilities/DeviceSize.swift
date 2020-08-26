b188f211d89b767fff673b566ed5e521aba1c972
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-26 17:33:36 +0900

    update DeviceSize

--- DailyCook/Utilities/DeviceSize.swift
+++ DailyCook/Utilities/DeviceSize.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct DeviceSize {
+    static var screenBounds: CGRect {
+        return UIScreen.main.bounds
+    }
+
+    static var screenWidth: CGFloat {
+        return UIScreen.main.bounds.size.width
+    }
+
+    static var screenHeight: CGFloat {
+        return UIScreen.main.bounds.size.height
+    }
+
+    static var statusBarHeight: CGFloat {
+        return UIApplication.shared.statusBarFrame.height
+    }
+
+    static func navBarHeight(_ navigationController: UINavigationController?) -> CGFloat {
+        return navigationController?.navigationBar.frame.size.height ?? 0
+    }
+
+    static func tabBarHeight(_ tabBarController: UITabBarController?) -> CGFloat {
+        return tabBarController?.tabBar.frame.size.height ?? 0
+    }
+}
