1cbc7adc1c280517b29225c1529c46817235e1ae
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-26 16:41:36 +0900

    create ios project

---
+++ DailyCook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DailyCook
+//
+//  Created by admin on 2020/08/26.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
