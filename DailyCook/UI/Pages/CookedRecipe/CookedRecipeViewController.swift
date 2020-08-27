a3ae131296c0cb5b320e948ad2a49e9deed68f57
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-27 15:02:16 +0900

    update CookedRecipeViewController

--- DailyCook/UI/Pages/CookedRecipe/CookedRecipeViewController.swift
+++ DailyCook/UI/Pages/CookedRecipe/CookedRecipeViewController.swift
@@ -6,4 +6,55 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+
+final class CookedRecipeViewController: UIViewController, View, ViewConstructor {
+    
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    
+    private let scrollView = UIScrollView().then {
+        $0.alwaysBounceVertical = true
+        $0.showsVerticalScrollIndicator = false
+    }
+    
+    private let stackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.alignment = .fill
+        $0.distribution = .fill
+    }
+    
+    // MARK: - Life Cycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(stackView)
+    }
+    
+    func setupViewConstraints() {
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        stackView.snp.makeConstraints {
+            $0.edges.equalTo(scrollView)
+        }
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: CookedRecipeReactor) {
+        // Action
+        
+        // State
+    }
+}
