1a4565cd4d44000912cc2b9f8dad431d58d0adff
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-27 12:26:41 +0900

    update RecipeDetailReactor

--- DailyCook/UI/Pages/RecipeDetail/RecipeDetailReactor.swift
+++ DailyCook/UI/Pages/RecipeDetail/RecipeDetailReactor.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+
+final class RecipeDetailReactor: Reactor {
+    enum Action {}
+    enum Mutation {}
+    
+    struct State {
+        let recipeDetail: RecipeDetail
+        
+        init(recipeDetail: RecipeDetail) {
+            self.recipeDetail = recipeDetail
+        }
+    }
+    
+    let initialState: State
+    
+    init() {
+        // TODO: Replace initial test data
+        initialState = State(recipeDetail: TestData.recipeDetail())
+    }
+}
