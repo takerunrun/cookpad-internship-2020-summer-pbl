13563fdd2531d4de4fb796cc13e4453a3850b657
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-27 12:58:07 +0900

    update RecipeDetailViewController

--- DailyCook/UI/Pages/RecipeDetail/RecipeDetailViewController.swift
+++ DailyCook/UI/Pages/RecipeDetail/RecipeDetailViewController.swift
@@ -6,4 +6,61 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+
+final class RecipeDetailViewController: UIViewController, View, ViewConstructor {
+    
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    private let contentScrollView = UIScrollView().then {
+        $0.showsVerticalScrollIndicator = false
+        $0.alwaysBounceVertical = true
+    }
+    
+    private let stackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.alignment = .fill
+        $0.distribution = .fill
+    }
+    
+    private let header = RecipeDetailHeaderView()
+    
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        view.addSubview(contentScrollView)
+        contentScrollView.addSubview(stackView)
+        stackView.addArrangedSubview(header)
+    }
+    
+    func setupViewConstraints() {
+        contentScrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        header.snp.makeConstraints {
+            $0.width.equalTo(DeviceSize.screenWidth)
+        }
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: RecipeDetailReactor) {
+        header.reactor = reactor
+        
+        // Action
+        
+        // State
+    }
+}
