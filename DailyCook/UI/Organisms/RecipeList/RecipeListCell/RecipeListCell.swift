a139da5d8d4374063625cb14f3418f9a14217245
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-27 11:04:50 +0900

    update RecipeListCell

--- DailyCook/UI/Organisms/RecipeList/RecipeListCell/RecipeListCell.swift
+++ DailyCook/UI/Organisms/RecipeList/RecipeListCell/RecipeListCell.swift
@@ -6,4 +6,89 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxCocoa
+
+final class RecipeListCell: UICollectionViewCell, View, ViewConstructor {
+    
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    private let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.layer.masksToBounds = true
+        $0.layer.cornerRadius = 4
+    }
+    
+    private let recipeNumberLabel = UILabel().then {
+        $0.apply(fontStyle: .regular, size: 14)
+        $0.textColor = Color.textBlack
+    }
+    
+    private let recipeNameLabel = UILabel().then {
+        $0.apply(fontStyle: .regular, size: 14)
+        $0.textColor = Color.textGray
+    }
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        addSubview(imageView)
+        addSubview(recipeNumberLabel)
+        addSubview(recipeNameLabel)
+    }
+    
+    func setupViewConstraints() {
+        imageView.snp.makeConstraints {
+            $0.top.left.right.equalToSuperview()
+        }
+        recipeNumberLabel.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(8)
+            $0.left.right.equalToSuperview()
+            $0.height.equalTo(16)
+        }
+        recipeNameLabel.snp.makeConstraints {
+            $0.top.equalTo(recipeNumberLabel.snp.bottom).offset(8)
+            $0.left.right.equalToSuperview()
+            $0.height.equalTo(16)
+            $0.bottom.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: RecipeListCellReactor) {
+        // Action
+        
+        // State
+        reactor.state.map { $0.recipe.imageUrl }
+            .distinctUntilChanged()
+            .bind { [weak self] imageUrl in
+                self?.imageView.setImage(imageUrl: imageUrl)
+            }
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.recipe.number }
+            .distinctUntilChanged()
+            .map { "#\($0)" }
+            .bind(to: recipeNumberLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.recipe.name }
+            .distinctUntilChanged()
+            .bind(to: recipeNameLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+}
