7efbbdb1adba9ecd2f3c9e9aff93944c852b16a1
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-27 13:52:40 +0900

    update CookedRecipe

--- DailyCook/BusinessLogic/Entity/CookedRecipe.swift
+++ DailyCook/BusinessLogic/Entity/CookedRecipe.swift
@@ -5,5 +5,9 @@
 //  Created by admin on 2020/08/27.
 //  Copyright © 2020 admin. All rights reserved.
 //
-
 import Foundation
+
+struct CookedRecipe: Equatable {
+    let date: Date
+    let imageUrl: String
+}
