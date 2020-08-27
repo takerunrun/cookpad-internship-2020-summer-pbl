087fa0cf3173ca8da0497b05130a5ad16c744023
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-27 14:54:00 +0900

    update CookedRecipeReactor

--- DailyCook/UI/Pages/CookedRecipe/CookedRecipeReactor.swift
+++ DailyCook/UI/Pages/CookedRecipe/CookedRecipeReactor.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+
+final class CookedRecipeReactor: Reactor {
+    enum Action {}
+    enum Mutation {}
+    
+    struct State {
+        var mainDishRecipeCellReactors: [RecipeListCellReactor] = TestData.recipes(count: 5).map { RecipeListCellReactor(recipe: $0) }
+    }
+    
+    let initialState = State()
+}
