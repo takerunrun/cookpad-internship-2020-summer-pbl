d6d786f642d358da312db64ef0e746da29f3c21d
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-28 08:25:05 +0900

    update ImageDataStore

--- DailyCook/BusinessLogic/DataStore/ImageDataStore.swift
+++ DailyCook/BusinessLogic/DataStore/ImageDataStore.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import FirebaseStorage
+import Firebase
+
+struct ImageDataStore {
+    private let storageReference: StorageReference
+    
+    init() {
+        self.storageReference = Storage.storage().reference()
+    }
+    
+    func createImage(imageData: Data, completion: @escaping ((Result<CookedRecipe, Error>) -> Void)) {
+        let fileName = "\(UUID()).jpg"
+        let imageRef = storageReference.child(fileName)
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpg"
+        
+        _ = imageRef.putData(imageData, metadata: metaData) { metadata, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                let cookedRecipe = CookedRecipe(date: Date(), imagePath: fileName)
+                completion(.success(cookedRecipe))
+            }
+        }
+    }
+}
