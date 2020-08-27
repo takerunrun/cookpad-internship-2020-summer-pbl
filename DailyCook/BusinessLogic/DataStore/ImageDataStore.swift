5bd3afe5f03a663af3597805c3fed202eba97c41
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-28 08:42:41 +0900

    add createCookedRecipe

--- DailyCook/BusinessLogic/DataStore/ImageDataStore.swift
+++ DailyCook/BusinessLogic/DataStore/ImageDataStore.swift
@@ -8,6 +8,7 @@
 
 import FirebaseStorage
 import Firebase
+import RxSwift
 
 struct ImageDataStore {
     private let storageReference: StorageReference
@@ -29,6 +30,27 @@ struct ImageDataStore {
                 let cookedRecipe = CookedRecipe(date: Date(), imagePath: fileName)
                 completion(.success(cookedRecipe))
             }
+        }
+    }
+    
+    func createCookedRecipe(imageData: Data) -> Observable<CookedRecipe> {
+        let fileName = "\(UUID()).jpg"
+        let imageRef = storageReference.child(fileName)
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpg"
+        
+        return Observable.create { (observer: AnyObserver<CookedRecipe>) -> Disposable in
+            
+            _ = imageRef.putData(imageData, metadata: metaData) { metadata, error in
+                if let error = error {
+                    observer.onError(error)
+                } else {
+                    let cookedRecipe = CookedRecipe(date: Date(), imagePath: fileName)
+                    observer.onNext(cookedRecipe)
+                }
+                observer.onCompleted()
+            }
+            return Disposables.create()
         }
     }
 }
