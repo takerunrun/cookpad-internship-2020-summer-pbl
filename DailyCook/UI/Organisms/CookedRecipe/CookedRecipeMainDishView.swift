e8f4c7f0797b4c39d0ed06418ee23a5a13820bca
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-27 15:33:12 +0900

    update CookedRecipeMainDishView

--- DailyCook/UI/Organisms/CookedRecipe/CookedRecipeMainDishView.swift
+++ DailyCook/UI/Organisms/CookedRecipe/CookedRecipeMainDishView.swift
@@ -13,7 +13,7 @@ import ReactorKit
 final class CookedRecipeMainDishView: UIView, View, ViewConstructor {
     
     private struct Const {
-        static let height: CGFloat = RecipeListCell.Const.cellHeight
+        static let height: CGFloat = 56 + RecipeListCell.Const.cellHeight
         static let width: CGFloat = DeviceSize.screenWidth
         static let collectionViewContentInset: UIEdgeInsets = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)
         static let minimumLineSpacing: CGFloat = 12
@@ -32,6 +32,20 @@ final class CookedRecipeMainDishView: UIView, View, ViewConstructor {
     }
     
     // MARK: - Views
+    private let titleLabel = UILabel().then {
+        $0.apply(fontStyle: .medium, size: 20)
+        $0.textColor = Color.textBlack
+        $0.text = "主菜"
+    }
+    
+    let showAllButton = UIButton().then {
+        $0.titleLabel?.apply(fontStyle: .regular, size: 16)
+        $0.setTitle("すべて見る", for: .normal)
+        $0.setTitleColor(Color.textBlack, for: .normal)
+        $0.setTitleColor(Color.lightGray, for: .highlighted)
+        $0.titleEdgeInsets.bottom = 0
+    }
+    
     private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout().then {
         $0.itemSize = Const.itemSize
         $0.minimumLineSpacing = Const.minimumLineSpacing
@@ -57,12 +71,23 @@ final class CookedRecipeMainDishView: UIView, View, ViewConstructor {
     
     // MARK: - Setup Methods
     func setupViews() {
+        addSubview(titleLabel)
+        addSubview(showAllButton)
         addSubview(collectionView)
     }
     
     func setupViewConstraints() {
+        titleLabel.snp.makeConstraints {
+            $0.left.equalToSuperview().inset(24)
+            $0.bottom.equalTo(collectionView.snp.top).offset(-12)
+        }
+        showAllButton.snp.makeConstraints {
+            $0.right.equalToSuperview().inset(24)
+            $0.bottom.equalTo(collectionView.snp.top).offset(-8)
+        }
         collectionView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+            $0.left.right.bottom.equalToSuperview()
+            $0.height.equalTo(RecipeListCell.Const.cellHeight)
         }
     }
     
