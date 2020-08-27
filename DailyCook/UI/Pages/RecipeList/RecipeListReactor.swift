59c08fdad48c74a368ba71f9ce225d7edf3d5c50
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-27 16:40:43 +0900

    add action and mutation to RecipeListReactor

--- DailyCook/UI/Pages/RecipeList/RecipeListReactor.swift
+++ DailyCook/UI/Pages/RecipeList/RecipeListReactor.swift
@@ -9,13 +9,37 @@
 import ReactorKit
 
 final class RecipeListReactor: Reactor {
-    enum Action {}
-    enum Mutation {}
+    enum Action {
+        case load
+    }
+    enum Mutation {
+        case setRecipeCellReactors([Recipe])
+    }
     
     struct State {
-        // TODO: Replace initial data
-        var recipeCellReactors: [RecipeListCellReactor] = TestData.recipes(count: 13).map { RecipeListCellReactor(recipe: $0) }
+        var recipeCellReactors: [RecipeListCellReactor] = []
     }
     
     let initialState = State()
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .load:
+            return loadRecipes().map(Mutation.setRecipeCellReactors)
+        }
+    }
+    
+    private func loadRecipes() -> Observable<[Recipe]> {
+        // TODO: Replace test data
+        return .just(TestData.recipes(count: 9))
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case let .setRecipeCellReactors(recipes):
+            state.recipeCellReactors = recipes.map { RecipeListCellReactor(recipe: $0) }
+        }
+        return state
+    }
 }
