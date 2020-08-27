89d9f9996e19ab441c083d6123dd5b0fcf61f94d
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-27 12:58:13 +0900

    update TabBar

--- DailyCook/UI/Pages/TabBar/TabBarController.swift
+++ DailyCook/UI/Pages/TabBar/TabBarController.swift
@@ -23,9 +23,12 @@ class TabBarController: UITabBarController {
     // MARK: - Setup Methods
     private func setupViewControllers() {
         viewControllers = [
-            RecipeListViewController().then {
-                $0.reactor = RecipeListReactor()
-            },
+//            RecipeListViewController().then {
+//                $0.reactor = RecipeListReactor()
+//            },
+            UINavigationController(rootViewController: RecipeDetailViewController().then {
+                $0.reactor = RecipeDetailReactor()
+            }),
             DevelopingViewController(type: "CookedRecipe"),
         ]
 
