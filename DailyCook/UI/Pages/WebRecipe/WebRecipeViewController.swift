455fd85e5821935c5a464f69b8261e06cb24fee4
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-27 17:31:30 +0900

    update WebRecipeViewController

--- DailyCook/UI/Pages/WebRecipe/WebRecipeViewController.swift
+++ DailyCook/UI/Pages/WebRecipe/WebRecipeViewController.swift
@@ -6,4 +6,67 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import WebKit
+import UIKit
+
+class WebRecipeViewController: UIViewController, ViewConstructor {
+    let webView = WKWebView()
+    let url = URL(string: "https://takerunrun.github.io/goshuingo-privacy-policy/")
+    
+    private var progressView = UIProgressView(progressViewStyle: .bar)
+    private var activityIndicatorView = UIActivityIndicatorView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView.navigationDelegate = self
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    func setupViews() {
+        view.addSubview(webView)
+        if let url = url {
+            let myRequest = URLRequest(url: url)
+            webView.load(myRequest)
+        }
+        activityIndicatorView.hidesWhenStopped = true
+        view.addSubview(activityIndicatorView)
+    }
+    
+    func setupViewConstraints() {
+        webView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        activityIndicatorView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    private func startIndicator() {
+        activityIndicatorView.startAnimating()
+    }
+
+    private func stopIndicator() {
+        activityIndicatorView.stopAnimating()
+    }
+}
+
+extension WebRecipeViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        startIndicator()
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        stopIndicator()
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        stopIndicator()
+    }
+
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        stopIndicator()
+    }
+}
