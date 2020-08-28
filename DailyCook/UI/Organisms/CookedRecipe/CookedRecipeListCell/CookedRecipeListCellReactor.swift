ac4649d5d8d948cc02fccd83281e74c82351ae42
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-28 12:07:26 +0900

    update CookedRecipeListCellReactor

--- DailyCook/UI/Organisms/CookedRecipe/CookedRecipeListCell/CookedRecipeListCellReactor.swift
+++ DailyCook/UI/Organisms/CookedRecipe/CookedRecipeListCell/CookedRecipeListCellReactor.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+
+final class CookedRecipeListCellReactor: Reactor {
+    enum Action {}
+    enum Mutation {}
+
+    struct State {
+        let recipe: Recipe
+        
+        init(recipe: Recipe) {
+            self.recipe = recipe
+        }
+    }
+    
+    var initialState: State
+    
+    init(recipe: Recipe) {
+        initialState = State(recipe: recipe)
+    }
+}
+
+extension CookedRecipeListCellReactor: Equatable {
+    static func == (lhs: CookedRecipeListCellReactor, rhs: CookedRecipeListCellReactor) -> Bool {
+        return lhs.currentState.recipe.id == rhs.currentState.recipe.id
+    }
+}
