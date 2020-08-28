4b1726687efaec708f25c8bf4c084a0af2a95211
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-08-28 10:27:31 +0900

    add action

--- DailyCook/UI/Pages/CookedRecipe/CookedRecipeReactor.swift
+++ DailyCook/UI/Pages/CookedRecipe/CookedRecipeReactor.swift
@@ -9,8 +9,14 @@
 import ReactorKit
 
 final class CookedRecipeReactor: Reactor {
-    enum Action {}
-    enum Mutation {}
+    enum Action {
+        case load
+    }
+    enum Mutation {
+        case setMainDish([Recipe])
+        case setSideDish([Recipe])
+        case setSoup([Recipe])
+    }
     
     struct State {
         // TODO: Replace initial test data
@@ -20,6 +26,33 @@ final class CookedRecipeReactor: Reactor {
     }
     
     let initialState = State()
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .load:
+            return .empty()
+        }
+    }
+    
+    private func loadCookedRecipes() -> Observable<([Recipe], [Recipe], [Recipe])> {
+        return .empty()
+    }
+    
+    
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case let .setMainDish(recipes):
+            state.mainDishRecipeCellReactors = recipes.map { RecipeListCellReactor(recipe: $0) }
+        case let .setSideDish(recipes):
+            state.sideDishRecipeCellReactors = recipes.map { RecipeListCellReactor(recipe: $0) }
+        case let .setSoup(recipes):
+            state.soupRecipeCellReactors = recipes.map { RecipeListCellReactor(recipe: $0) }
+        }
+        return state
+        
+    }
     
     func createRecipeDetailFromMainDish(indexPath: IndexPath) -> RecipeDetailReactor {
         let recipe = currentState.mainDishRecipeCellReactors[indexPath.row].currentState.recipe
